a869be7b110bdc5c0251c078e39a2ee1ce73b8ea
Nguyễn Văn Khải <khai20041802@gmail.com>
2025-07-10 12:43:03 +0700

    Update ingiaithua.asm

--- CODE DE CUONG/xulysohoc/ingiaithua.asm
+++ CODE DE CUONG/xulysohoc/ingiaithua.asm
@@ -1,68 +1,90 @@
+inchuoi macro message
+    mov ah, 9
+    lea dx, message
+    int 21h
+endm  
+
 .model small
 .stack 100h
 .data
-    tb1 db "Nhap n : $"
-    tb2 db 10, 13, "Ket qua n! = $"
+    msg_nhap db 'Nhap so n (< 8) = $'
+    msg_kq   db 10, 13, 'Ket qua n! = $'
+    b  dw 10
+    num      dw ?
 .code
+
 main proc
     mov ax, @data
     mov ds, ax
-    
-    mov ah, 9
-    lea dx, tb1
-    int 21h
-    
-    mov ah, 1
-    int 21h
-    cmp al, '0'
-    je dacbiet
-    sub al, '0'
-    mov cl, al
-    mov bl, 1
-    mov al, 1
+    mov es, ax
 
-giaithua:
-    mul bl
-    cmp cl, bl
-    je chiakq
-    inc bl
-    jmp giaithua
+    inchuoi msg_nhap
 
-chiakq:
-    mov bx, 10
-    xor cx, cx
-chia:
-    xor dx, dx  
-    div bx
-    push dx
-    inc cx
-    cmp ax, 0
-    jne chia
+    call nhap_num
 
-hienthi:
-    mov ah, 9
-    lea dx, tb2
-    int 21h
-lap:
-    xor dx, dx
-    pop dx
-    add dl, '0'
-    mov ah, 2
-    int 21h
-    loop lap
-    jmp thoat
-dacbiet:
-    mov ah, 9
-    lea dx, tb2
-    int 21h
-    
-    mov dl, '1'
-    int 21h
-thoat:
+    call tinh_giai_thua
+
+    inchuoi msg_kq   
+    mov ax, num
+    call in_ket_qua
+
     mov ah, 4ch
     int 21h
 main endp
-end main
-    
-    
-    
+
+tinh_giai_thua proc
+    mov cx, num    
+    mov ax, 1        
+
+tinh:
+    mul cx        
+    dec cx
+    cmp cx, 1
+    jne tinh
+    mov num, ax       
+    ret
+tinh_giai_thua endp
+
+
+nhap_num proc
+    mov num, 0
+
+nhap_lai:
+    mov ah, 1
+    int 21h
+    cmp al, 13  
+    je ket_thuc_nhap
+    sub al, '0'
+    mov bl, al
+
+    mov ax, num
+    mul b
+    add ax, bx
+    mov num, ax
+    jmp nhap_lai
+
+ket_thuc_nhap:
+    ret
+nhap_num endp
+
+in_ket_qua proc
+    xor cx, cx
+
+chia10:
+    xor dx, dx
+    div b
+    push dx
+    inc cx
+    cmp ax, 0
+    jne chia10
+
+hien_thi:  
+    pop dx   
+    add dx, '0'
+    mov ah, 2
+    int 21h
+    loop hien_thi
+
+    ret
+in_ket_qua endp
+end
