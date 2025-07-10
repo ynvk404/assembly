b9c143bd0f88c8d5dddeb84220e6947b8b22efce
Nguyễn Văn Khải <khai20041802@gmail.com>
2025-07-10 15:55:07 +0700

    Update demsonguyento.asm

--- CODE DE CUONG/xulysohoc/demsonguyento.asm
+++ CODE DE CUONG/xulysohoc/demsonguyento.asm
@@ -1,35 +1,28 @@
 .model small
 .stack 100h
 .data
-    tb1 db "Nhap vao 1 chuoi : $"
-    tb2 db 10, 13, "So luong so nguyen to : $"
+    tb1 db "Nhap vao 1 chuoi so : $"
+    tb2 db 10, 13, "So luong so nguyen to la : $"
     snt dw 0
-    string db 100, 0, 100 dup(0)
 .code
 main proc
     mov ax, @data
     mov ds, ax
-    
     mov ah, 9
     lea dx, tb1
     int 21h
-    
-    call nhap
-    
+    call nhap 
     mov ah, 9
     lea dx, tb2
     int 21h
-    
     mov ax, snt
     call inso
-    
     mov ah, 4ch
     int 21h
 main endp
-
-nhap proc
-    xor dx, dx
+nhap proc   
     xor cx, cx
+    xor dx, dx
 nhapktra:
     mov ah, 1
     int 21h
@@ -37,7 +30,6 @@ nhapktra:
     je kiemtra
     cmp al, 13
     je exit
-
     sub al, '0'
     mov cl, al
     mov ax, dx
@@ -46,24 +38,22 @@ nhapktra:
     add ax, cx
     mov dx, ax
     jmp nhapktra
-
 kiemtra:
     call checknto
     xor dx, dx
     jmp nhapktra
 exit:
-    cmp dx, 0   
+    cmp dx, 0
     je boqua
     call checknto
 boqua:
     ret
-nhap endp   
-
+nhap endp
 checknto proc
     cmp dx, 2
     je lasnt
     cmp dx, 1
-    je notsnt
+    jbe notsnt
     
     mov bx, 2
 lap:
@@ -74,13 +64,12 @@ lap:
     je notsnt
     inc bx
     cmp bx, ax
-    jl lap
+    jbe lap
 lasnt:
     inc snt
 notsnt:
     ret
 checknto endp
-
 inso proc
     xor cx, cx
     mov bx, 10
@@ -93,13 +82,12 @@ chia10:
     jne chia10
 hienthi:
     pop dx
-    mov ah, 2
     add dl, '0'
+    mov ah, 2
     int 21h
     loop hienthi
-ret  
 inso endp
-
 end main
-;can than truoc khi chia bx phai xor dx, dx
-; va can than dung cho mov bx, 2 vo vong lap
+    
+    
+    
